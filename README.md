e6ef652a34f100b78c7136b1a8025e105ea8fb83
Christopher Kennedy <chris94kennedy@gmail.com>
2019-08-05 01:49:56 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Generalized Map and Reduce Lab
+ b# Generalized Map and Reduce Lab
 
 ## Learning Goals
 
--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+def map(source_array)
+  new = []
+  i = 0 
+  while i < source_array.length do
+    new << yield(source_array[i])
+    i += 1
+  end
+  return new
+end
+  
+  
+  
+def reduce(source_array, starting_point=nil)
+ if starting_point = nil
+   i = 1
+   new = source_array[i]
+ else
+   i = 0
+   new = starting_point
+ end
+  while i < source_array.length do 
+   new = yield(new, source_array[i])
+   i += 1 
+ end
+ return new
+ end
